b8c2c7638b1451e19130a20b9e9b3d1b6b3ad132
weliton <weliton.ww@hotmail.com>
2023-06-15 16:48:49 -0300

    cofigure-eslint-cpmplete

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
   variant?: ButtonVariant
 }
 
-export function Button({variant = 'primary'}: ButtonProps) {
+export function Button({ variant = 'primary' }: ButtonProps) {
   return (
     <>
       <ButtonContainer variant={variant}>Enviar</ButtonContainer>
