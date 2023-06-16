c13b6baa6bd08e9e09eb075cec362073d2244a0b
weliton <weliton.ww@hotmail.com>
2023-06-16 14:31:26 -0300

    layout rotes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './components/styles/themes/default'
 import { GlobalStyle } from './components/styles/global'
 
-import { Router } from './components/Router'
+import { Router } from './Router'
 import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
